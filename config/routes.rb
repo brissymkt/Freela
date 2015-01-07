2af367d3f27a1e045101fe6df045e7c150ebac60
Wellington Calligaris <wcalligaris@gmail.com>
2015-01-07 21:30:08 -0200

    configured devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root "devise/sessions#new"
-  devise_for :users do 
-    root :to => "devise/sessions#new"
+  devise_for :users
+  devise_scope :user do 
+    authenticated :user do
+      root :to => "home#home", :as => :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => "devise/sessions#new", :as => :unauthenticated_root
+    end
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
