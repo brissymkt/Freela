e79af76b4942eb6ccad064f6b7ec35c3167d62ca
Wellington Calligaris <wcalligaris@gmail.com>
2015-01-28 21:56:06 -0200

    counting number of environmental analyses to allow creation of new one

--- app/models/environmental_analysis.rb
+++ app/models/environmental_analysis.rb
@@ -22,6 +22,7 @@ class EnvironmentalAnalysis < ActiveRecord::Base
 	validates :grade, :presence => true
 	validates :type_of_analysis, :presence => true, :inclusion => {:in => VALID_TYPES }
 	validate :year_and_month_in_the_future?
+	validate :enough_existing_analysis?
 
 	accepts_nested_attributes_for :factors, :allow_destroy => true, :reject_if => lambda{ |attributes| attributes[:name].blank?}
 
@@ -36,6 +37,19 @@ class EnvironmentalAnalysis < ActiveRecord::Base
 
 	def set_year_and_date_to_beginning_of_month
 		self.year_and_month = self.year_and_month.beginning_of_month
+	end
+
+	def enough_existing_analysis?
+		if self.type_of_analysis == WEEKLY_ANALYSIS
+			errors.add(:type_of_analysis, I18n.t('activerecord.errors.enough_analyses')) if EnvironmentalAnalysisCounter.analyses_for_user_in_period(self.user, self.year_and_month.beginning_of_week, self.year_and_month.end_of_week) > 1
+		elsif self.type_of_analysis == MONTHLY_ANALYSIS
+			errors.add(:type_of_analysis, I18n.t('activerecord.errors.enough_analyses')) if EnvironmentalAnalysisCounter.analyses_for_user_in_period(self.user, self.year_and_month.beginning_of_month, self.year_and_month.end_of_month) > 1
+		elsif self.type_of_analysis == TRIMONTHLY_ANALYSIS
+			errors.add(:type_of_analysis, I18n.t('activerecord.errors.enough_analyses')) if EnvironmentalAnalysisCounter.analyses_for_user_in_period(self.user, self.year_and_month.beginning_of_month, self.year_and_month.end_of_month) > 1
+		elsif self.type_of_analysis == ANNUAL_ANALYSIS
+			errors.add(:type_of_analysis, I18n.t('activerecord.errors.enough_analyses')) if EnvironmentalAnalysisCounter.analyses_for_user_in_period(self.user, self.year_and_month.beginning_of_year, self.year_and_month.end_of_year) > 1
+		end
+			
 	end
 
 end
---
+++ app/models/environmental_analysis_counter.rb
@@ -0,0 +1,9 @@
+#coding: utf-8
+
+class EnvironmentalAnalysisCounter
+
+	def self.analyses_for_user_in_period(user, start_date, end_date)
+		EnvironmentalAnalysis.where(:user_id => user.id, :year_and_month => start_date..end_date).count
+	end
+
+end
