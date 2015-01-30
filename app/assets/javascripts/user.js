a9e8834ccddf57bdc5945f7c26fa0d72592668c1
Wellington Calligaris <wcalligaris@gmail.com>
2015-01-30 00:07:39 -0200

    still some problems with css on edit page

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -11,4 +11,24 @@ $(document).ready(function() {
 		return false;
 	});
 
+	$('.factors_container').on('click', '.contract_button', function() {
+		if ($(this).hasClass('glyphicon-arrow-down')) {
+			$(this).removeClass('glyphicon-arrow-down');
+			$(this).addClass('glyphicon-arrow-up');
+			$(this).parents('.factor_text_inputs_container').addClass('expanded');
+		} else {
+			$(this).removeClass('glyphicon-arrow-up');
+			$(this).addClass('glyphicon-arrow-down');
+			$(this).parents('.factor_text_inputs_container').removeClass('expanded');
+		}
+	});
+
+	$('.factors_container').on('click', '.edit_button', function() {
+		if ($(this).parents('.factor_text_inputs_container').hasClass('edit')) {
+			$(this).parents('.factor_text_inputs_container').removeClass('edit');
+		} else {
+			$(this).parents('.factor_text_inputs_container').addClass('edit');
+		}
+	});
+
 });
