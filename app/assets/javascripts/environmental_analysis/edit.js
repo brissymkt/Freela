5e2ad3759a65aa9fcb900462958398e15b72243a
Wellington Calligaris <wcalligaris@gmail.com>
2015-01-30 22:32:53 -0200

    custom slider

---
+++ app/assets/javascripts/environmental_analysis/edit.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	// Create slider input to existing factors
+	$('.js_slider').slider({
+		value: $(this).parents('.importance_container').find('.js_importance_input').val(),
+		min: 0,
+		max: 10,
+		step: 1,
+		slide: function(event, ui) {
+			$(this).parents('.importance_container').find('.js_importance_input').val(ui.value);
+			$(this).parents('.importance_container').find('.js_slider_caption').html(ui.value);
+		}
+	});
+
+	updateSliders();
+
+	//Function to add sliders. If slider exists, it will be updated. Otherwise, a slider will be greated
+	$('.factors_container').on('cocoon:after-insert', function(e, insertedElement){
+		$(insertedElement).find('.js_slider').slider({ 
+			value: $(this).parents('.importance_container').find('.js_importance_input').val(),
+			min: 0,
+			max: 20,
+			step: 1,
+			slide: function(event, ui) {
+				$(this).parents('.importance_container').find('.js_importance_input').val(ui.value);
+				$(this).parents('.importance_container').find('.js_slider_caption').html(ui.value);
+			}
+		});
+		updateSliders();
+	});
+
+	//Update sliders with values stored in database
+	function updateSliders() {
+		$('.js_slider').each(function(){
+			$(this).slider('option', 'value', $(this).parents('.importance_container').find('.js_importance_input').val());
+			$(this).parents('.importance_container').find('.js_slider_caption').html($(this).slider('value'));
+		});
+	}
+});
