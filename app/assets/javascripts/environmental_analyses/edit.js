41018bd81fb8aa8d8e1964e76228eaa4c9f111bf
Wellington Calligaris <wcalligaris@gmail.com>
2015-02-28 23:51:00 -0300

    placebo button working in environmental analysis edit page

--- app/assets/javascripts/environmental_analyses/edit.js
+++ app/assets/javascripts/environmental_analyses/edit.js
@@ -17,7 +17,28 @@ $(document).ready(function(){
 		window.location.href = $(this).find('.js-link-to-factor').attr('href');
 	});
 
+	$('.factors-container input, .factors-container .placebo-button').click(function() {
+		return false;
+	});
 
+	$('.factors-container input').change(function() {
+		$(this).parents('.othsys-input-hoverable').find('.placebo-button').addClass('js-input-touched');
+	});
+
+	$('.factors-container .placebo-button').click(function() {
+		var newName = $(this).parents('.text-inputs-container').find('.js-name-input').val();
+		$(this).removeClass('js-input-touched');
+		$(this).parents('.text-inputs-container').removeClass('edit');
+		$(this).parents('.text-inputs-container').find('.factor-name-display').html(newName);
+	});
+
+	$('.factors-container').on('click', '.placebo-button', function() {
+		var temporaryName = $(this).parents('.text-inputs-container').find('.js-name-input').val();
+		$(this).addClass('submitted');
+		$(this).parents('.text-inputs-container').find('input').addClass('hidden');
+		$(this).parents('.text-inputs-container').find('.only-inputs-box').addClass('compacted-box');
+		$(this).parents('.text-inputs-container').find('.factor-name-display').html(temporaryName);
+	});
 	//Function to add sliders. If slider exists, it will be updated. Otherwise, a slider will be greated
 	$('.factors-container').on('cocoon:after-insert', function(e, insertedElement){
 		$(insertedElement).find('.js-slider').slider({ 
