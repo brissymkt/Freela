47efa8d3ec432bf9ad2ec7840015e98e63117fec
Wellington Calligaris <wcalligaris@gmail.com>
2015-03-08 14:58:53 -0300

    minor fixes

--- app/models/sub_factor.rb
+++ app/models/sub_factor.rb
@@ -3,8 +3,8 @@ class SubFactor < ActiveRecord::Base
 	belongs_to :factor
 	validates :factor_id, :presence => true
 	validates :name, :presence => true, :length => {:minimum => 3}
-	validates :importance, :presence => true, :inclusion => {:in => (1..10).each.map {|allowed_value| allowed_value}}
-	validates :situation, :presence => true, :inclusion => {:in => (1..10).each.map { |allowed_value| allowed_value}}
+	validates :importance, :presence => true, :inclusion => {:in => (0..10).each.map {|allowed_value| allowed_value}}
+	validates :situation, :presence => true, :inclusion => {:in => (0..10).each.map { |allowed_value| allowed_value}}
 
 	before_save :calculate_grade
 
