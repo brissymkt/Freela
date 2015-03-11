e6d8a94b04dfc5f0b3fb96a4e7a03bd387f4a544
Wellington Calligaris <wcalligaris@gmail.com>
2015-03-10 21:41:11 -0300

    chart working properly

--- app/assets/javascripts/user/environmental_analyses/chart.js
+++ app/assets/javascripts/user/environmental_analyses/chart.js
@@ -1,23 +1,27 @@
 yojs.define('OthsysUser.user.environmental_analyses.chart', function() {
-	var chartOptions = {};
-	var day, month, year;
-	day = parseInt($('.js-market-environment-chart-data .start-date .day').html().trim());
-	month = parseInt($('.js-market-environment-chart-data .start-date .month').html().trim());
-	year = parseInt($('.js-market-environment-chart-data .start-date .year').html().trim());
-	chartOptions.suffix = $('.js-market-environment-chart-data .suffix').html().trim();
-	chartOptions.title = $('.js-market-environment-chart-data .title').html().trim();
-	chartOptions.content = [];
-	var data = [];
-	$('.js-market-environment-chart-data .data').each(function() {
-		data.push(parseFloat($(this).html()));
-	});
-	chartOptions.content.push({
-		data: data,
-		name: $('.js-market-environment-chart-data .series-name').html().trim(),
-		pointInterval: 7 * 24 * 3600 * 1000,
-		pointStart: new Date(year, month - 1, day).getTime()
-	});
-	console.log(new Date(year, month - 1, day).getTime());
-	console.log(chartOptions.content);
-	yojs.call('OthsysUser.user.chart', '.js-market-environment-chart', chartOptions);
+	if ($('body').attr('data-locale') == 'pt-BR') {
+		yojs.call('OthsysUser.user.chart-ptBR');
+	}
+	if ($('.js-market-environment-chart-data').length > 0){
+		var chartOptions = {};
+		var day, month, year;
+		day = parseInt($('.js-market-environment-chart-data .start-date .day').html().trim());
+		month = parseInt($('.js-market-environment-chart-data .start-date .month').html().trim());
+		year = parseInt($('.js-market-environment-chart-data .start-date .year').html().trim());
+		chartOptions.suffix = $('.js-market-environment-chart-data .suffix').html().trim();
+		chartOptions.title = $('.js-market-environment-chart-data .title').html().trim();
+		chartOptions.content = [];
+		var data = [];
+		$('.js-market-environment-chart-data .data').each(function() {
+			data.push(parseFloat($(this).html()));
+		});
+		chartOptions.content.push({
+			data: data,
+			name: $('.js-market-environment-chart-data .series-name').html().trim(),
+			pointInterval: 7 * 24 * 3600 * 1000,
+			pointStart: new Date(year, month - 1, day).getTime(),
+			negativeColor: '#FF0000'
+		});
+		yojs.call('OthsysUser.user.chart', '.js-market-environment-chart', chartOptions);
+	}
 });
--- app/assets/javascripts/user/shared/chart.js
+++ app/assets/javascripts/user/shared/chart.js
@@ -2,13 +2,35 @@
 yojs.define('OthsysUser.user.chart', function(chartSelector, attributes) {
 	$(chartSelector).highcharts({
 		chart: {
-			zoomType: 'x'
+			zoomType: 'x',
+			resetZoomButton: {
+        position: {
+          x: 0,
+          y: -30
+        },
+        theme: {
+          fill: 'white',
+          stroke: 'silver',
+          r: 0,
+          states: {
+            hover: {
+              fill: '#29B1AB',
+              style: {
+                color: 'white'
+              }
+            }
+          }
+      	}
+    	}
 		},
 		credits: {
 			enabled: false
 		},
 		title: {
 			text: attributes.title
+		},
+		subtitle: {
+			text: $('.environmental-chart-subtitle').html().trim()
 		},
 		yAxis: {
 			title: {
@@ -35,4 +57,30 @@ yojs.define('OthsysUser.user.chart', function(chartSelector, attributes) {
 		},
 		series: attributes.content
 	});
+});
+
+yojs.define('OthsysUser.user.chart-ptBR', function() {
+	if ($('body').attr('data-locale') == 'pt-BR') {
+		Highcharts.setOptions({
+			lang: {
+	      loading: 'Aguarde...',
+	      months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+	      weekdays: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+	      shortMonths: ['Jan', 'Feb', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+	      exportButtonTitle: "Exportar",
+	      printButtonTitle: "Imprimir",
+	      rangeSelectorFrom: "De",
+	      rangeSelectorTo: "Até",
+	      rangeSelectorZoom: "Periodo",
+	      downloadPNG: 'Download imagem PNG',
+	      downloadJPEG: 'Download imagem JPEG',
+	      downloadPDF: 'Download documento PDF',
+	      downloadSVG: 'Download imagem SVG',
+	      resetZoom: "Voltar à escala original",
+	      resetZoomTitle: "Reset",
+	      thousandsSep: ".",
+	      decimalPoint: ','
+	    }
+		});
+	}
 });
